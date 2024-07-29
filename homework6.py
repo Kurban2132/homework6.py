24180b1bd4fdd30b81c555e98906a6c0e1887fb0
Kurban2132 <k.kokanov@yandex.ru>
2024-07-29 11:00:04 +0300

    Add files via upload

--- homework6.py
+++ homework6.py
@@ -5,11 +5,13 @@ print(my_dict["Kurban"])
 my_dict["Kurban"] = 1968
 print(my_dict['Kurban'])
 my_dict["Maxim"] = 1982
-print(my_dict["Maxim"])
+print(my_dict)
+print(my_dict.get("Nikolay", "Такого ключа нет"))
 my_dict.update({"Alex": 1979, "Sergey": 1975})
 print(my_dict)
-del my_dict["Alex"]
+a = my_dict.pop("Alex")
 print(my_dict)
+print(a)
 set_ = {1, 2, 3, 4, 5, 6, 3, 5, "Anton", "Sergey", (4, 3, 1, 2)}
 print(set_)
 set_.update([7, 8, 9])
